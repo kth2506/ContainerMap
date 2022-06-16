9370ee9826453dcd138bb12b0d7cf3a291c9c828
kim taehwan <ksj4159@naver.com>
2022-06-16 12:49:52 +0900

    // ** ContainerMap ver0.2 06.16

--- ContainerMap/Main.cpp
+++ ContainerMap/Main.cpp
@@ -1,37 +1,47 @@
-// ** ContainerMap ver0.1 06.15
+// ** ContainerMap ver0.2 06.16
 
 #include <iostream>
+#include <string>
 #include <map>
 
 using namespace std;
 
+map<int, int> Numbers;
+
+void AddData(int _Key , int _Value);
+
 int main(void)
 {
+	Numbers[1] = 10;
+	Numbers[2] = 10;
+
+	Numbers.insert(make_pair(0, 30));
+	Numbers.insert(make_pair(3, 30));
+	Numbers.insert(make_pair(4, 40));
 	
+	AddData(2, 200);
 
-	map<string, int> Numbers2;
-
-	// ** Key, Value
-	//map<int, int>
-	Numbers2["A"] = 1;
-	Numbers2["B"] = 2;
-	Numbers2["C"] = 3;
-	Numbers2["D"] = 4;
-	Numbers2["E"] = 5;
-	for (auto iter = Numbers2.begin(); iter != Numbers2.end(); ++iter)
-	{
-		cout << iter->first << endl;
+	for (map<int, int>::iterator iter = Numbers.begin();
+		iter != Numbers.end(); ++iter)
 		cout << iter->second << endl;
-	}
 
 
-	
 	return 0;
+}
+
+void AddData(int _Key, int _Value)
+{
+	map<int, int>::iterator iter = Numbers.find(_Key);
+
+	if (iter == Numbers.end())
+		Numbers.insert(make_pair(_Key, _Value));
+	else
+			iter->second = _Value;
 }
 
 /*
 map<string, int> Numbers2;
-
+이진 트리구조
 // ** Key, Value
 //map<int, int>
 Numbers2["A"] = 1;
